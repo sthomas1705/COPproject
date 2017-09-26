c687b2997aa7f46ca07350095da5ec4de88b500b
sthomas1705 <stephen.thomas@ufl.edu>
2017-09-26 09:04:30 -0400

    Lab 4 guit test

---
+++ Lab 4/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int calcScore;
+       calcScore=0;
+       System.out.println("I hate calc 2");
+       System.out.println("I'm gonna get a:"+" "+ calcScore);
+
+
+
+
+
+    }
+}
